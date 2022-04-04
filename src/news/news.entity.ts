ec023998f89022cfd83ab79a9b383242319cfb5e
Kirill Makhov <mmahovkirill@gmail.com>
2022-04-04 23:36:39 +0300

    feat: create news scheme

--- src/news/news.entity.ts
+++ src/news/news.entity.ts
@@ -1,1 +1,37 @@
-export class NewsEntity {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { CategoriesEntity } from '../categories/categories.entity';
+import { UsersEntity } from '../users/users.entity';
+
+@Entity('news')
+export class NewsEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('text', { nullable: true })
+  cover: string;
+
+  @ManyToOne(() => CategoriesEntity, (category) => category.news)
+  category: CategoriesEntity;
+
+  @ManyToOne(() => UsersEntity, (user) => user.news)
+  user: UsersEntity;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
