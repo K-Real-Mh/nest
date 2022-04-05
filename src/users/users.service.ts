396b850db33f1a56e0c5e80591b07cb1dd5d337e
Kirill Makhov <mmahovkirill@gmail.com>
2022-04-06 00:10:42 +0300

    feat: add method for create new user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsersEntity } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly usersRepository: Repository<UsersEntity>,
+  ) {}
+
+  // Возвращаемое значение может быть Promise<UsersEntity|undefined>
+  // Озвучить устно, что необходимо отработать крайний случай на уровне выше, если запись не произошла
+  async create(user): Promise<UsersEntity | undefined> {
+    const userEntity = new UsersEntity();
+    userEntity.firstName = user.firstName;
+    userEntity.lastName = user.lastName;
+    userEntity.email = user.email;
+    userEntity.role = user.role;
+    return await this.usersRepository.save(userEntity);
+  }
+}
